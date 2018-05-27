30795e0c04b0def7e980f45ad76b017dbb3e35d5
Gustaf <gustaaf_97@hotmail.com>
2018-05-27 05:09:02 +0200

    Scramble
    Har börjat pilla med vue + användning av async/await for att kunna skapa cool randomiser effekt. Har även lagt in en algo som blandar arrays...

--- smashapp.js
+++ smashapp.js
@@ -75,9 +75,34 @@ var app = new Vue
   },
   methods:
   {
-    scramble: function()
+    scramble: async function()
     {
-      //this.leftboxImg = this.character[0].img;
+      let loops = 0;
+      let ms = 75;
+      var shuffleArray = function(array)
+      {
+        var j, x, i;
+        for (i = array.length - 1; i > 0; i--)
+        {
+          j = Math.floor(Math.random() * (i + 1));
+          x = array[i];
+          array[i] = array[j];
+          array[j] = x;
+        }
+      }
+      let pause = function(ms) //A lil pause fx() via async/await (ms = pause time)
+      {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      shuffleArray(app.character);
+      do //Do this loop while loops are less than...
+        for (var i = 0; i < this.character.length; i++, loops++)
+        {
+          await pause(ms);
+          this.leftboxImg = this.character[i].img;
+          ms *= 1.025;
+        }
+      while (loops < 30);
     },
   },
 })
